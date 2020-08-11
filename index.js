f9a88004f9e1c2a41829f1fb4a96e70824713265
Jose Jimenez <58991130+jjdevco@users.noreply.github.com>
2020-08-11 13:48:04 -0500

    Integrate custom error handler

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-
+const errorHandler = require("./middlewares/errorHandler");
 // Routes
 const postRoutes = require("./routes/posts");
 
@@ -14,6 +14,9 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/api/post", postRoutes);
+
+// Error Handler Middleware
+app.use(errorHandler);
 
 // Listen PORT
 const port = process.env.PORT || 5000;
